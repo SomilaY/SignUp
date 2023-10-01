8a72ed8e5ed8047e0a4c86f5b4a89f120fc4c43c
SomilaY <ST10091012@vcconnect.edu.za>
2023-10-01 13:18:05 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt'); 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB 
+mongoose.connect('mongodb+srv://SomilaY:Thekingishere16@somilacluster.kul1amk.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Create a User schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  surname: String,
+  username: String,
+  password: String, // Store hashed passwords
+  email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// API endpoint for user registration
+app.post('/register', async (req, res) => {
+  try {
+    // Validate the required fields
+    const { name, surname, username, password, email } = req.body;
+    if (!name || !surname || !username || !password || !email) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 is the saltRounds
+
+    // Sanitize input data
+    const sanitizedUser = {
+      name: name.trim(),
+      surname: surname.trim(),
+      username: username.trim(),
+      password: hashedPassword, // Store the hashed password
+      email: email.trim().toLowerCase(),
+    };
+
+    // Create a new user with sanitized data
+    const user = new User(sanitizedUser);
+    
+    // Save the user to the database
+    await user.save();
+    
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Registration failed' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
